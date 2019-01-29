b1b5a92abc0e8ba06c7849c5f52aac60ceb1a63c
altbdoor <lancersupraskyline@gmail.com>
2019-01-29 20:04:07 +0800

    added the test content stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestContentComponent } from './test-content/test-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestContentComponent
   ],
   imports: [
     BrowserModule
